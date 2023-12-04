2d5bec8fb79f99ba49faaad5c73d92bef8457cbd
Francesca <carellafrancesca29@gmail.com>
2023-12-04 15:20:03 +0100

    esercizi

--- Abbonamenti_Palestra/AbbonamentiPalestra - FrontEnd/abbonamentiPalestra/src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { HomepageComponent } from './homepage/homepage.component';
-import { ActivityComponent } from './activity/activity.component';
-
-export const routes: Routes = [
-  { path: '', component: HomepageComponent },
-  { path: 'homepage', component: HomepageComponent },
-  { path: 'activity', component: ActivityComponent },
-];
