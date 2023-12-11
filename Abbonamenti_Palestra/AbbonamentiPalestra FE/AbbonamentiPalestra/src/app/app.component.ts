859a653100b1a48483be87b26267a3f73227bfc1
Francesca <carellafrancesca29@gmail.com>
2023-12-11 18:00:50 +0100

    esercizi FE

--- Abbonamenti_Palestra/AbbonamentiPalestra FE/AbbonamentiPalestra/src/app/app.component.ts
+++ Abbonamenti_Palestra/AbbonamentiPalestra FE/AbbonamentiPalestra/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [HttpClientModule, CommonModule, RouterOutlet, RouterLink, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
