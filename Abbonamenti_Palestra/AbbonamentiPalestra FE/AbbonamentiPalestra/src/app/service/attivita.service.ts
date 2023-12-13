1af2f4f5c959e9013bad646816cc1306110d4594
Francesca <carellafrancesca29@gmail.com>
2023-12-13 09:31:34 +0100

    python

---
+++ Abbonamenti_Palestra/AbbonamentiPalestra FE/AbbonamentiPalestra/src/app/service/attivita.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AttivitaService {
+
+  private attivitaUrl = 'http://localhost:8080/api/tipi/attivita';
+
+  constructor(private http: HttpClient) { }
+
+  getTipiAttivita(): Observable<string[]>{
+    return this.http.get<string[]>(this.attivitaUrl);
+  }
+
+}
