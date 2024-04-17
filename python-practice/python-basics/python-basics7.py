683187bcfdc487b1b431b2c6dd68332887bba9c4
Francesca Carella <francesca.carella@reddoak.com>
2024-04-17 17:56:49 +0200

    python basics

--- python-practice/python-basics/python-basics7.py
+++ python-practice/python-basics/python-basics7.py
@@ -5,6 +5,7 @@ class Event:
         self.event_name = event_name
         self.event_date = event_date
         self.location = location
+        self.seats_available = 100
 
 class EventBookingSystem:
     def __init__(self):
@@ -12,6 +13,18 @@ class EventBookingSystem:
     
     def add_event(self, event):
         self.event_list.append(event)
+
+    def book_a_seat(self, event_name, seats_available_num):
+        for event in self.event_list:
+            if event_name == event_name:
+                if event.seats_available >= seats_available_num:
+                    event.seats_available -= seats_available_num
+                    print(f"Prenotazione effettuata per {seats_available_num} posti all'evento {event_name}")
+                else:
+                    print("Spiacenti, non ci sono abbastanza posti disponibili per questo evento.")
+            return
+        print(f"Evento '{event_name}' non trovato.")
+
     
     def show_event_list(self):
         print("Lista degli eventi in programmazione")
